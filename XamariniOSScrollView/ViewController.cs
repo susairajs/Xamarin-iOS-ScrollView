92fdb4c4eb2c8bddae4db8690a26a676e8e30058
DelpinSusaiRaj <susairajs@outlook.com>
2017-05-09 09:56:03 +0530

    All files Added

---
+++ XamariniOSScrollView/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+using Foundation;
+
+namespace XamariniOSScrollView
+{
+	public partial class ViewController : UIViewController
+	{
+		UIScrollView scrollView;
+		UIImageView imageView;
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			scrollView = new UIScrollView(new CGRect(0, 0, View.Frame.Width, View.Frame.Height));
+			View.AddSubview(scrollView);
+
+			imageView = new UIImageView(UIImage.FromFile("Xamarin.jpg"));
+			scrollView.ContentSize = imageView.Image.Size;
+			scrollView.AddSubview(imageView);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
